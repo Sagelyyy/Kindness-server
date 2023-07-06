d3d7954717620c759e673dd9b953d8267684035b
Chris Weiskopf <chris@chriscancode.io>
2023-07-06 19:36:45 -0400

    tweaking admin check

--- src/utils/admin.js
+++ src/utils/admin.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const adminCheck = (req, res, next) => {
   const adminIps = [process.env.ADMINIP, process.env.DEVIP];
-  const userIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const userIp = req.headers["x-forwarded-for"];
 
   if (adminIps.includes(userIp)) {
     req.admin = true;
