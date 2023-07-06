886985ee57fc34751496c285d8ba5fe9802bc7c9
Chris Weiskopf <chris@chriscancode.io>
2023-07-05 22:48:10 -0400

    Tweaking admin check

--- src/utils/admin.js
+++ src/utils/admin.js
@@ -4,8 +4,9 @@ dotenv.config();
 export const adminCheck = (req, res, next) => {
   const adminIps = [process.env.ADMINIP, process.env.DEVIP];
   const userIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const adminKey = localStorage.getItem("admin");
 
-  if (adminIps.includes(userIp)) {
+  if (adminIps.includes(userIp) || adminKey == process.env.ADMINKEY) {
     req.admin = true;
   } else {
     req.admin = false;
